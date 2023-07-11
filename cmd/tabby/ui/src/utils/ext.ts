0011c04a2d856364c8884da12828afff179dd693
alex.dou <alex.dou@qq.com>
2023-07-11 14:57:46 +0800

    增加后缀名-->icon的映射

---
+++ cmd/tabby/ui/src/utils/ext.ts
@@ -0,0 +1,42 @@
+const ext = {
+    "default": "icon-file", // default file
+
+    // txt
+    "txt": "icon-txt",
+    "csv": "icon-file-csv",
+    "pdf": "icon-file-pdf",
+
+    // office
+    "doc": "icon-word",
+    "docx": "icon-word",
+    "ppt": "icon-ppt",
+    "pptx": "icon-ppt",
+    "xls": "icon-excel",
+    "xlsx": "icon-excel",
+
+    // image
+    "png": "icon-file-png",
+    "gif": "icon-f-gif",
+    "bmp": "icon-bmp",
+    "svg": "icon-svg",
+    "jpeg": "icon-JPEG",
+    "jpg": "icon-JPEG",
+
+    // stream
+    "avi": "icon-avi",
+    "flv": "icon-FLV",
+    "mp4": "icon-mp",
+
+    // code
+    "go": "icon-go",
+    "vue": "icon-Vue",
+    "ts": "icon-typescript",
+    "sql": "icon-file-sql",
+
+    "css": "icon-css",
+    "jar": "icon-jar",
+    "html": "icon-HTML",
+    "js": "icon-js",
+    "json": "icon-json",
+};
+export default ext
